603d7213bd5ecbce01c1b49475cb61a07f161fc4
AnhTuanPham69 <anhtuanpham.dev@gmail.com>
2021-12-26 03:34:37 +0700

    Done part-4

--- app.js
+++ app.js
@@ -1,24 +1,31 @@
-require('dotenv').config();
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+require("dotenv").config();
 
-const db = require('./config/db/connect');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const db = require("./config/db/connect");
+
+const indexRouter = require("./src/routes/index");
+const usersRouter = require("./src/routes/users");
+const wikiRouter = require("./src/routes/wiki");
+const catalogRouter = require('./src/routes/catalog'); 
 
 const app = express();
+
+// Connect to database
 db.connect();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/wiki", wikiRouter);
+app.use('/catalog', catalogRouter);  // Add catalog routes to middleware chain.
 
 module.exports = app;
---
+++ src/models/genre.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const GenreSchema= new Schema({
+  name: { type: String, required: true, maxlength: 100, minlength: 3 },
+});
+
+// Virtual for genre's URL
+GenreSchema
+.virtual('url')
+.get(function () {
+    return '/catalog/genre/' + this._id;
+})
+
+//Export model
+module.exports = mongoose.model('Genre', GenreSchema);
