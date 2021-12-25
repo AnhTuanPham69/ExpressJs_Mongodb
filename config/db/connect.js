4a9e151b2d7f053bed214a1078ea83697c03e105
AnhTuanPham69 <anhtuanpham.dev@gmail.com>
2021-12-26 02:30:05 +0700

    done part-3

--- config/db/connect.js
+++ config/db/connect.js
@@ -1,12 +1,20 @@
+require("dotenv").config();
+
 const mongoose = require("mongoose");
 
+const connection_string = process.env.CONNECTION_STRING;
 async function connect() {
   try {
-    await mongoose.connect('mongodb://localhost:27017/users');
+    await mongoose.connect(connection_string, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connect database success!!!");
   } catch (error) {
-      console.log(error)
-    console.log("Connect database failure!!!");
+    console.log(`Connect database failure!!!
+     ${error}`);
+
+    console.log("Connection String: " + connection_string);
   }
 }
 
